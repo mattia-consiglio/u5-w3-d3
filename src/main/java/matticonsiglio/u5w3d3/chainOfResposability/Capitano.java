836b6f97a2bebea8e7c76b408f6e255a6e5abb5f
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-04-17 17:34:38 +0200

    feat(chain-of-responsibility): implement salary checks
    - Make Capitano.stipendio private
    - Implement CapitanoFilter to check salary against Capitano
    - Add ChainRunner to wire filters and test salary checks
    - Add Colonnello class and ColonnelloFilter
    - Other minor refactorings

---
+++ src/main/java/matticonsiglio/u5w3d3/chainOfResposability/Capitano.java
@@ -0,0 +1,8 @@
+package matticonsiglio.u5w3d3.chainOfResposability;
+
+import lombok.Getter;
+
+@Getter
+public class Capitano extends Ufficiale {
+    public final int stipendio = 2000;
+}
---
+++ src/main/java/matticonsiglio/u5w3d3/chainOfResposability/Filter.java
@@ -0,0 +1,2 @@
+package matticonsiglio.u5w3d3.chainOfResposability;public class Filter {
+}
