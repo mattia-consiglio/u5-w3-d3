e222537b4b3068f48d78380a84f1b496b1c69fbd
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-04-17 14:52:35 +0200

    first commit

---
+++ src/main/java/matticonsiglio/u5w3d3/U5W3D3Application.java
@@ -0,0 +1,13 @@
+package matticonsiglio.u5w3d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D3Application.class, args);
+	}
+
+}
---
+++ src/test/java/matticonsiglio/u5w3d3/U5W3D3ApplicationTests.java
@@ -0,0 +1,13 @@
+package matticonsiglio.u5w3d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
